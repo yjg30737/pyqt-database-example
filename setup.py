6fb60b655d2e5602146aba03491e6c49b89e4cca
Jung Gyu Yoon <yjg30737@gmail.com>
2022-08-06 17:39:51 +0900

    Modify description just a little

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt Database (mainly SQLite currently) basic use example',
+    description='PyQt database (mainly SQLite currently) basic use example',
     url='https://github.com/yjg30737/pyqt-database-example.git',
     install_requires=[
         'PyQt5>=5.8',
